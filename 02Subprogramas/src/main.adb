9c49df8c59b2119b122a7b6ecb0b30183e64b291
jonathanmg96 <jonathan.moncada@edu.uah.es>
2022-11-10 15:10:40 +0100

    Subida3

--- 02Subprogramas/src/main.adb
+++ 02Subprogramas/src/main.adb
@@ -36,22 +36,24 @@ procedure Main is
                Put (C);
          end case;
       end loop;
-      Put_Line("");
+      Put_Line ("");
    end Hacker_lane;
 
-   function Area_rectangulo (Lado_mayor : Float;Lado_menor : Float) return Float is
+   function Area_rectangulo
+     (Lado_mayor : Float; Lado_menor : Float) return Float
+   is
    begin
-      return Lado_mayor*Lado_menor;
+      return Lado_mayor * Lado_menor;
    end Area_rectangulo;
-   Area: Float;
-   procedure Intercambiar(A:in out Integer; B : in out Integer) is
-      Primero:Integer:=A;
-      Segundo:Integer:=B;
+   Area : Float;
+   procedure Intercambiar (A : in out Integer; B : in out Integer) is
+      Primero : Integer := A;
+      Segundo : Integer := B;
    begin
-      A:=Segundo;
-      B:=Primero;
+      A := Segundo;
+      B := Primero;
    end Intercambiar;
-   A : Integer:=5;
+   A : Integer := 5;
    B : Integer := 10;
 begin
    --  Insert code here.
@@ -60,11 +62,10 @@ begin
    --    (Text => "Segunda forma de llamar subprograma!", Caracter => '=');
    --  Reverse_Line ("Hola");
    --  Hacker_lane ("Hola");
-   Area:= Area_rectangulo(2.0,4.0);
-   Put(Area, Exp => 0, Aft => 2);
-   Put_Line("Antes del cambio:" &A'Image&" "&B'Image);
-   Intercambiar(A,B);
-   Put_Line("Despues del cambio anterior:" &A'Image&" "&B'Image);
+   Area := Area_rectangulo (2.0, 4.0);
+   Put (Area, Exp => 0, Aft => 2);
+   Put_Line ("Antes del cambio:" & A'Image & " " & B'Image);
+   Intercambiar (A, B);
+   Put_Line ("Despues del cambio anterior:" & A'Image & " " & B'Image);
    null;
 end Main;
-
